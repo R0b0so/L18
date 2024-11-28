0fe351cefe3c7226325009b96e7482f6cc2c776d
R0b0so <ethansagar222@gmail.com>
2024-11-27 18:47:58 -0800

    hello

---
+++ time2.py
@@ -0,0 +1,6 @@
+from datetime import datetime as DT 
+Time = DT.now()
+print("Hour = ", Time.hour)
+print("Minute = ", Time.minute)
+print("Second = ", Time.second)
+print("Microsecond = ", Time.microsecond)
